c5e9c93c616bbf92cd7a53d9bf92811fabcac120
merttsvnc <merttsvnc@gmail.com>
2023-10-04 21:28:11 +0900

    add user name to dashboard page

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const { ensureAuthenticated } = require("../config/auth")
 
 router.get("/", (req, res) => res.render("welcome"))
-router.get("/dashboard", ensureAuthenticated, (req, res) => res.render("dashboard"))
+router.get("/dashboard", ensureAuthenticated, (req, res) => res.render("dashboard", {
+    name: req.user.name
+}))
 
 module.exports = router
