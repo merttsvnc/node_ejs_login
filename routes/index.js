1b81225863e63abff4165e16b40b666cd314ade8
merttsvnc <merttsvnc@gmail.com>
2023-10-04 18:12:21 +0900

    added auth and protected dashboard page

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const router = express.Router()
+const { ensureAuthenticated } = require("../config/auth")
 
 router.get("/", (req, res) => res.render("welcome"))
-router.get("/dashboard", (req, res) => res.render("dashboard"))
+router.get("/dashboard", ensureAuthenticated, (req, res) => res.render("dashboard"))
 
 module.exports = router
